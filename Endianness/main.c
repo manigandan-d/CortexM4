bb9f9e69aabff39165bb10e7a708cc0db3d2e9f4
manigandan-d <manigandan220916@gmail.com>
2025-11-29 19:22:31 +0530

    Check the processor endiannes - little/big endian

--- Endianness/main.c
+++ Endianness/main.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// Define AIRCR (Application Interrupt and Reset Control Register) address
-#define SCB_AIRCR (*(volatile uint32_t *)0xE000ED0C)
-
 int main(void) {
-    // Read the AIRCR register
-    uint32_t aircr = SCB_AIRCR;
+	uint32_t *SCB_AIRCR = (uint32_t *)0xE000ED0C;
 
-    // Bit 15 of AIRCR indicates endianness:
-    // 0 = Little-endian, 1 = Big-endian
-    uint32_t endianness = (aircr >> 15) & 0x1;
+	uint32_t endianness = (*SCB_AIRCR >> 15) & 0x1;
+
+	printf("Endiannes value: %ld\n", endianness);
 
     if (endianness == 0) {
         printf("Processor is Little-endian\n");
-    } 
-    
+    }
+
     else {
         printf("Processor is Big-endian\n");
     }
 
-    while(1); 
+	while(1);
 }
